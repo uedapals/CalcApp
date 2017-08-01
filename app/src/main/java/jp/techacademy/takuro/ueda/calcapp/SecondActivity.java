8b1cfa613040bcfabf2d69d023d0be2cc87ab5e4
uedapals <ueda@palsbots.net>
2017-08-02 00:12:31 +0900

    諸々修正

--- app/src/main/java/jp/techacademy/takuro/ueda/calcapp/SecondActivity.java
+++ app/src/main/java/jp/techacademy/takuro/ueda/calcapp/SecondActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-import android.util.Log;
 public class SecondActivity extends AppCompatActivity {
 
     @Override
